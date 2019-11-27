2e75f94d4a817e6b6e44a67f880b3f223b143303
Kenneth <Kenneth@DESKTOP-8O89D8D.fritz.box>
2019-11-27 16:20:22 +0100

    - added a test class for StackAsList

---
+++ src/rpn_lab6/TestStackAsList.java
@@ -0,0 +1,55 @@
+package rpn_lab6;
+
+public class TestStackAsList {
+
+	StackAsList myList;
+	
+	Node n1, n2, n3, n4;
+	
+	public TestStackAsList() throws StackOverflow {
+		testAdd();
+	}
+	
+	public static void main (String args[]) throws StackOverflow 
+	{
+		new TestStackAsList();
+	}
+	
+	/**
+	 * Test cases:
+	 * 
+	 * -> add Objects to the list
+	 * -> pop Objects from the list
+	 * -> print out List
+	 * -> get top element of the list
+	 * @throws StackOverflow 
+	 */
+
+	
+	private void testAdd() throws StackOverflow 
+	{
+		// create the list
+		myList = new StackAsList();
+		
+		// create the nodes
+		createNodes();
+		
+		// printing put the empty list
+		System.out.println(myList.toString());
+		
+		// adding nodes to it
+		myList.push(n1);
+		myList.push(n2);
+		
+		System.out.println(myList.toString());
+		
+	}
+	
+	private void createNodes() 
+	{
+		n1 = new Node(42);
+		n2 = new Node(13);
+		n3 = new Node("foo");
+		n4 = new Node("bar");
+	}
+}
