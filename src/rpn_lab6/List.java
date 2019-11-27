709cd6bf03ea335f47bf59df4e762ee2952bafc4
Kenneth <Kenneth@DESKTOP-8O89D8D.fritz.box>
2019-11-27 16:00:06 +0100

    - created List class - created StackAsLIst class - hope everything works properly

--- src/rpn_lab6/List.java
+++ src/rpn_lab6/List.java
@@ -2,15 +2,99 @@ package rpn_lab6;
 
 public class List {
 
-	private Node first;
+	private Node head, current, previous;
+	private int size;
 	
 	public List() {
-		
+		head = null;
+		current = null;
+		previous = null;
+		size = 0;
 	}
 	
-	public List(Node n) 
+	/*
+	 * Adding an element to the top of the List.
+	 * No options for putting it at the end or in the middle, 
+	 * because we don't need it for this task.
+	 */
+	public void add(Object obj) 
 	{
-		first = n;
+		Node n = new Node(obj, head);
+		head = n;
+		size += 1;
+		
+		reset();
 	}
 
+	public void remove() 
+	{
+//		if (isEmpty() || current == null) 
+//		{
+//			return;
+//		}
+//		else 
+//		{
+			if(previous == null) 
+			{
+				head = current.next;
+				current = head;
+			}
+			else 
+			{
+				previous.next = current.next;
+				current = current.next;
+			}
+//		}
+		size -= 1;
+	}
+	
+	public boolean isEmpty() 
+	{
+		return head == null;
+	}
+	
+	public int getSize() 
+	{
+		return size;
+	}
+	
+	public void moveOn() 
+	{
+		current = current.next;
+		if (previous == null)
+			previous = head;
+		else
+			previous = previous.next;
+	}
+	
+	public boolean isLastMember() 
+	{
+		return (current == null);
+	}
+	
+	public Node firstElement() 
+	{
+		return head;
+	}
+	
+	public void reset() 
+	{
+		current = head;
+		previous = null;
+	}
+	
+	/**
+	 * Taken from layout and edited
+	 * @credits https://people.f4.htw-berlin.de/~weberwu/info2/Handouts/List.java
+	 */
+	public String print(String open, String close) 
+	{	
+		String s = (open == "")?"[ ":open;
+		for (reset(); !isLastMember(); this.moveOn()){
+			// this wastes a lot of system data
+			s = s + current.data + ((current.next != null)?", ":"");
+		}
+		return s + ((close == "")?" ]":close);
+	}
+	
 }
--- src/rpn_lab6/StackAsList.java
+++ src/rpn_lab6/StackAsList.java
@@ -5,27 +5,40 @@ public class StackAsList implements Stack {
 	private List stackList;
 	
 	public StackAsList() {
-		this.Empty();
+		Empty();
 	}
 
-	public void push(Object o) throws StackOverflow {
-
+	public void push(Object obj) throws StackOverflow {
+		// not throwing any exception here, because the list is infinite and 
+		// there can't be a StackOverflow unless the memory is full
+		stackList.add(obj);
 	}
 
 	public void pop() throws StackUnderflow {
-
+		if (stackList.isEmpty()) 
+			throw new StackUnderflow();
+		else 
+			stackList.remove();
 	}
 
 	public Object top() throws StackUnderflow {
-		return null;
+		if (stackList.isEmpty())
+			throw new StackUnderflow();
+		else
+			return stackList.firstElement();
 	}
 
 	public boolean isEmpty() {
-		return false;
+		return stackList.isEmpty();
 	}
 
 	public void Empty() {
 		stackList = new List();
 	}
 
+	public String toString() 
+	{
+		return stackList.print("Top: ", " :Bottom");
+	}
+	
 }
