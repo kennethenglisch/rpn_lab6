31f4b88628b9fa244ae9b8314017089050fda5e1
Tony4711 <tony.schultze96@gmail.com>
2019-11-28 21:51:36 +0100

    Completet infixToPostfix, started with evaluation
    infixToPostfix is done but not yet tested properly.
    Evaluation has still an overflow, not sure why.

--- src/rpn_lab6/Postfix.java
+++ src/rpn_lab6/Postfix.java
@@ -4,77 +4,141 @@ public class Postfix {
 
 	private StackAsList postFixStack;
 	private StackAsList expression;
-
-	public Postfix() {
-		// TODO Auto-generated constructor stub
-	}
+	final private Character[] operator = { '+', '-', '*', '/', '^' };
 
 	public static void main(String[] args) throws StackUnderflow, StackOverflow {
-		String ifx = "1*2-3*2";
+		String ifx = "1+2*3";
 		String rpn = "";
 		Postfix fix = new Postfix();
 		rpn = fix.infixToPostfix(ifx);
 		System.out.println(rpn);
+		System.out.println(fix.evaluate(rpn));
 	}
 
-	public int evaluate(String pfx) {
+	public int evaluate(String pfx) throws StackOverflow, StackUnderflow {
 		expression = new StackAsList();
-		int n = 0;
-		int i = 0;
-		int length = pfx.length();
+		int a = 0;
+		char op = Character.MIN_VALUE;
+		int result = 0;
+		String lhs = "";
+		String rhs = "";
 
-		for (n = length; n > 0; n--) {
-			int end = (length - n) + 1;
+		for (int n = pfx.length(); n > 0; n--) {
 
-			String h = pfx.substring(i, end);
+			for (int i = 0; i < operator.length; i++) {
+				Character c = pfx.charAt(a);
 
-			if (!(h.equals("+") || h.equals("-") || h.equals("*") || h.equals("/"))) {
-				int value = Integer.parseInt(h);
+				if (Character.isLetterOrDigit(c)) {
+					expression.push(c);
+				} else if (c == operator[i]) {
+					switch (c) {
+					case '+':
+						op = '+';
+						break;
+					case '-':
+						op = '-';
+						break;
+					case '*':
+						op = '*';
+						break;
+					case '/':
+						op = '/';
+						break;
+					case '^':
+						op = '^';
+						break;
 
+					}
+					if (!postFixStack.isEmpty()) {
+						lhs = lhs + popTop(expression);
+						if (!postFixStack.isEmpty())
+							rhs = rhs + popTop(expression);
+						result = calculate(op, Integer.parseInt(lhs), Integer.parseInt(rhs));
+						expression.push((char) (result + '0'));
+					}
+				}
+				a++;
 			}
 		}
-
 		return 0;
+	}
+
+	public int calculate(char c, int a, int b) {
+		int eva = 0;
+		switch (c) {
+		case '+':
+			eva = a + b;
+		case '-':
+			eva = a - b;
+		case '*':
+			eva = a * b;
+		case '/':
+			eva = a / b;
+		case '^':
+			double base = a;
+			double expo = b;
+			eva = (int) Math.pow(base, expo);
+
+		}
+		return eva;
 	}
 
 	public String infixToPostfix(String ifx) throws StackUnderflow, StackOverflow {
 		postFixStack = new StackAsList();
 		String postFix = "";
-		int n = 0;
 		int i = 0;
-		int length = ifx.length();
 
-		for (n = length; n > 0; n--) {
-			int end = (length - n) + 1;
+		for (int n = ifx.length(); n > 0; n--) {
 
-			String h = ifx.substring(i, end);
+			Character c = ifx.charAt(i);
 
-
-				if (!(h.equals("+") || h.equals("-") || h.equals("*") || h.equals("/"))) {
-
-					postFix += h;
-				}
-				if (!postFixStack.isEmpty()) {
-					if (postFixStack.top().equals("+") || postFixStack.top().equals("-")
-							|| postFixStack.top().equals("*") || postFixStack.top().equals("/")) {
-						if (h.equals("+") || h.equals("-")) {
-							postFix += postFixStack.top();
-							postFixStack.pop();
-							postFixStack.push(h);
-						}
-					}
-				} else if (h.equals("+") || h.equals("-") || h.equals("*") || h.equals("/")) {
-					postFixStack.push(h);
+			if (Character.isLetterOrDigit(c)) {
+				postFix += c;
+			} else if (c.equals('('))
+				postFixStack.push(c);
+			else if (c == ')') {
+				while (!postFixStack.isEmpty() && !postFixStack.top().equals('('))
+					postFix += popTop(postFixStack);
+				if (!postFixStack.isEmpty() && !postFixStack.top().equals('('))
+					return "Invalid Expression";
+				else
+					postFixStack.pop();
+			} else {
+				while (!postFixStack.isEmpty() && checkOperator(c) <= checkOperator((char) postFixStack.top())) {
+					if (postFixStack.top().equals('('))
+						return "Invalid Expression";
+					postFix += popTop(postFixStack);
 				}
 
-				i++;
+				postFixStack.push(c);
 			}
-		
+
+			i++;
+		}
+
 		if (!postFixStack.isEmpty()) {
-			postFix += postFixStack.top();
-			postFixStack.pop();
+			postFix += popTop(postFixStack);
 		}
 		return postFix;
+	}
+
+	private int checkOperator(Character c) {
+		for (int i = 0; i < operator.length; i++)
+			if (c == operator[i]) {
+				if (c == '+' || c == '-')
+					return 0;
+				if (c == '*' || c == '-')
+					return 1;
+				if (c == '^')
+					return 2;
+			}
+		return -1;
+	}
+
+	private Object popTop(StackAsList stack) throws StackUnderflow {
+		Object o = stack.top();
+		stack.pop();
+		return o;
 	}
 
 }
