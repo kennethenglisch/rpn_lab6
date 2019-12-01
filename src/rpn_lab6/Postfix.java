68de504d3282784383636a2293bd76fde24beaa3
Tony4711 <tony.schultze96@gmail.com>
2019-12-01 21:50:41 +0100

    Update Postfix.java
    User can inser letters for variables.
    Bug fix for invalid expression return.
    Bug fix for StackUnderflow in infixToPostfix() when there is now '(' but a ')'

--- src/rpn_lab6/Postfix.java
+++ src/rpn_lab6/Postfix.java
@@ -8,16 +8,15 @@ public class Postfix {
 	private StackAsList postFixStack;
 	private StackAsList expression;
 	final private Character[] operator = { '+', '-', '*', '/', '^' };
+	private Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) throws StackUnderflow, StackOverflow {
-		/*String ifx = "1+2+3/4+5+6*(7+8)";
-		String rpn = "";
-		
-		rpn = fix.infixToPostfix(ifx);
-		System.out.println(rpn);
-		System.out.println(fix.evaluate(rpn));*/
 		Postfix fix = new Postfix();
-		System.out.println(fix.evaluate(fix.infixToPostfix(fix.readInfix())));
+		String post = fix.infixToPostfix(fix.readInfix());
+		if (post == "Invalid Expression")
+			System.out.println(post);
+		else
+			System.out.println(fix.evaluate(post));
 	}
 
 	public int evaluate(String pfx) throws StackOverflow, StackUnderflow {
@@ -65,19 +64,29 @@ public class Postfix {
 		postFixStack = new StackAsList();
 		String postFix = "";
 		int a = -1;
+		boolean open = false;
+		boolean close = false;
 
 		for (int n = ifx.length(); n > 0; n--) {
-
 			Character c = ifx.charAt(++a);
 
-			if (Character.isLetterOrDigit(c)) {
+			if (Character.isLetter(c)) {
+				System.out.println("Enter a value for the variable " + c);
+				String variable = scanner.nextLine().replaceAll(" ", "");
+				c = variable.charAt(0);
+				if (Character.isLetter(c))
+					return "Invalid Expression";
+			}
+			if (Character.isDigit(c)) {
 				postFix += c;
-			} else if (c.equals('('))
+			} else if (c.equals('(')) {
 				postFixStack.push(c);
-			else if (c == ')') {
+				open = true;
+			} else if (c == ')') {
+				close = true;
 				while (!postFixStack.isEmpty() && !postFixStack.top().equals('('))
 					postFix += popTop(postFixStack);
-				if (!postFixStack.isEmpty() && !postFixStack.top().equals('('))
+				if (!postFixStack.isEmpty() && !postFixStack.top().equals('(') || !open)
 					return "Invalid Expression";
 				else
 					postFixStack.pop();
@@ -93,6 +102,8 @@ public class Postfix {
 		while (!postFixStack.isEmpty()) {
 			postFix += popTop(postFixStack);
 		}
+		if (open != close)
+			return "Invalid Expression";
 		System.out.println("Postfix");
 		System.out.println("> " + postFix);
 		return postFix;
@@ -128,29 +139,51 @@ public class Postfix {
 		stack.pop();
 		return o;
 	}
-	
-	private String readInfix() throws StackOverflow, StackUnderflow 
-	{
-		Scanner scanner = new Scanner(System.in);
+
+	private String readInfix() throws StackOverflow, StackUnderflow {
+		scanner = new Scanner(System.in);
 		boolean finished = false;
 		System.out.println("Enter an infix expression.");
 		System.out.print("> ");
 		String ifx = scanner.nextLine().replaceAll(" ", "");
-		
-		while (!finished) 
-		{
-			if (ifx.matches(".*[a-zA-Z].*") || ifx.equals("") || ifx == null)
-			{
-				System.out.println("The infix expression you have entered is unvalid. You should not use any letters. Enter a new one. ");
+
+		while (!finished) {
+			if (ifx.equals("") || ifx == null) {
+				System.out.println(
+						"The infix expression you have entered is unvalid. You should not use any letters. Enter a new one. ");
 				System.out.print("> ");
 				ifx = scanner.nextLine().replaceAll(" ", "");
-			}
-			else finished = true;
+			} else
+				finished = true;
 		}
-		
-		scanner.close();
-		
 		return ifx;
+	}
+
+	/*
+	 * Method for multi digit numbers, not yet implemented. Needs to be implemented
+	 * into an iterator of a String that takes the current char and the next char
+	 * and hand it to this method as parameters. 
+	 * Problems: 
+	 * - could cause an OutOfBounce Exception for the char next when there is no guard
+	 * - returns a String of the multi digit number that is
+	 * 	splittet into single digit number term which needs to be written in the same
+	 *	position where the multi digit number was
+	 */
+	private String multiDigit(char current, char next) {
+		int count = 0;
+		String infix = "";
+
+		if (Character.isDigit(current) && Character.isDigit(next)) {
+			String multi = multiDigit(current, next);
+		}
+		String digit = String.valueOf(Character.getNumericValue(current)) + (Character.getNumericValue(next));
+		int number = Integer.parseInt(digit);
+		while (number > 9) {
+			number -= 9;
+			count++;
+		}
+		infix = "(9" + "*" + count + "+" + number + ")";
+		return infix;
 	}
 
 }
